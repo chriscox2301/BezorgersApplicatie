2adaa44d6653d4f8a75d1be3c049440379f1d111
Bram-Starren <2504125starren@zuyd.nl>
2026-06-16 09:42:35 +0200

    Initializer aangemaakt

--- BezorgApplicatie/Data/DataContextInitializer.cs
+++ BezorgApplicatie/Data/DataContextInitializer.cs
@@ -18,13 +18,46 @@ namespace BezorgApplicatie.Data
             var drivers = new Driver[]
             {
                 new Driver {Name = "Piet"},
-                new Driver {Name = "Henk"}
+              
             };
             context.Drivers.AddRange(drivers);
 
-            context.SaveChanges();
+            var vehicles = new Vehicle[] {
+                new Vehicle {Id = 1, LicensePlate = "AA-392-B" },
+                new Vehicle {Id = 2, LicensePlate = "BB-333-C" },
+                new Vehicle {Id = 3, LicensePlate = "DD-322-D" },
+                new Vehicle {Id = 4, LicensePlate = "VD-421-E" }
+            };
+            context.Vehicles.AddRange(vehicles);
+
+            var warehouses = new Warehouse[] {
+                new Warehouse {Location = "Maastricht" }, 
+                new Warehouse {Location = "Sittard" }
+            };
+
+            context.Warehouses.AddRange(warehouses); 
+            var loads = new Load[]
+                {
+                   new Load { Warehouse = warehouses[0] }  
+                };
+
+            context.Loads.AddRange(loads);
+
+            
+
+            var shift = new Shift[]
+            {
+                new Shift {StartTime = DateTime.Parse("2026-06-16 9:30:00"), EndTime = DateTime.Parse("2026-06-16 16:30:00"), Driver = drivers[0], Vehicle = vehicles[1],  Warehouse = warehouses[1],  Load = loads[0] },
+                new Shift {StartTime = DateTime.Parse("2026-06-18 9:30:00"), EndTime = DateTime.Parse("2026-06-18 16:30:00"), Driver = drivers[0], Vehicle = vehicles[3], Warehouse = warehouses[0],  Load = loads[0] }
+            };
+
+            context.Shifts.AddRange(shift);
 
             context.Database.EnsureCreated();
+
+            context.SaveChanges(); 
+
+
         }
     }
 }
