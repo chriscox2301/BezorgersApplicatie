417cc406c90edb338c621a1d99f236b452bd1f24
FabianRoso <2502026roso@zuyd.nl>
2026-06-16 16:32:49 +0200

    - Filter en zoek functie toegevoegd + klikken op order/stop

--- BezorgApplicatie/Views/AllStopsPage.xaml.cs
+++ BezorgApplicatie/Views/AllStopsPage.xaml.cs
@@ -2,34 +2,91 @@ using BezorgApplicatie.Data;
 using BezorgApplicatie.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.ObjectModel;
+using System.Security.Cryptography.X509Certificates;
 
 namespace BezorgApplicatie.Views;
 
 public partial class AllStopsPage : ContentPage
 {
     private readonly DataContext _dataContext;
-    public ObservableCollection<Order> Stops { get; set; } = new ObservableCollection<Order>();
+    public ObservableCollection<Order> AlleStops { get; set; } = new ObservableCollection<Order>();
+
+    public string OrderStatus = "Onderweg";
+    public string ZoekTerm = "";
 	public AllStopsPage(DataContext dataContext)
 	{
 		InitializeComponent();
         _dataContext = dataContext;
         BindingContext = this;
         LaadStops();
+        OrderStatus = "Onderweg";
+        Filter();
+        
 	}
 
     public async void LaadStops()
     {
         var stops = await _dataContext.Orders
                         .Include(s => s.Packages)
-                        //.Where(s => s.Date == DateTime.Now)
                         .ToArrayAsync();
 
-        Stops.Clear();
+        AlleStops.Clear();
 
         foreach (var stop in stops)
         {
-            Stops.Add(stop);
+            AlleStops.Add(stop);
         }
+    }
+
+    private void Filter()
+    {
+        var FilteredStops = AlleStops.Where(s => s.Status == OrderStatus);
+
+        if (!string.IsNullOrWhiteSpace(ZoekTerm))
+        {
+            FilteredStops = FilteredStops.Where(s => s.Address.ToLower().Contains(ZoekTerm)
+            || s.Id.ToString().ToLower().Contains(ZoekTerm)
+            || s.Packages.Any(p => p.Barcode.ToLower().Contains(ZoekTerm))
+            );
+        }
+
+        StopsList.ItemsSource = FilteredStops.ToList();
+    }
+
+    private void ToDoClicked(object sender, EventArgs e)
+    {
+        OrderStatus = "Onderweg";
+        Filter();
+    }
+
+    private void CompletedClicked(object sender, EventArgs e)
+    {
+        OrderStatus = "Bezorgd";
+        Filter();
+    }
+
+    private void DifferentClicked(object sender, EventArgs e)
+    {
+        OrderStatus = "Anders";
+        Filter();
+    }
+
+    private void ZoektermIngevuld(object sender, TextChangedEventArgs e)
+    {
+        ZoekTerm = e.NewTextValue.ToLower();
+        Filter();
+    }
+
+    private async void StopSelected(object sender, SelectionChangedEventArgs e)
+    {
+        var order = e.CurrentSelection.FirstOrDefault() as Order;
+
+        if (order == null)
+            return;
+
+        StopsList.SelectedItem = null;
+
+        await Navigation.PushAsync(new StopDetailPage(order));
     }
 
 }
---
+++ BezorgApplicatie/Views/StopDetailPage.xaml.cs
@@ -0,0 +1,11 @@
+using BezorgApplicatie.Models;
+namespace BezorgApplicatie.Views;
+
+
+public partial class StopDetailPage : ContentPage
+{
+	public StopDetailPage(Order order)
+	{
+		InitializeComponent();
+	}
+}
