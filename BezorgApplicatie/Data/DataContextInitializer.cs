f6175c3fcca7b38fd6118ea666cfc47d5725b639
Bonck05 <2303862hendriks@zuyd.nl>
2026-06-19 13:35:39 +0200

    Data handling toegevoegd in Ruwe lijnen met AI. Happy flow is nu mogelijk, moet dus alleen nog genuanceerd worden voor latere stages

--- BezorgApplicatie/Data/DataContextInitializer.cs
+++ BezorgApplicatie/Data/DataContextInitializer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BezorgApplicatie.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 
@@ -19,6 +20,50 @@ namespace BezorgApplicatie.Data
             }
 
             context.Database.EnsureCreated();
+
+            // Add test data if database is empty
+            if (!context.Packages.Any())
+            {
+                // First create a warehouse
+                var warehouse = new Warehouse 
+                { 
+                    Location = "Test Warehouse"
+                };
+                context.Warehouses.Add(warehouse);
+                context.SaveChanges();
+
+                // Then create a cart
+                var cart = new Cart
+                {
+                    WarehouseId = warehouse.Id,
+                    VehicleZone = "D"
+                };
+                context.Carts.Add(cart);
+                context.SaveChanges();
+
+                // Then create an order
+                var order = new Order
+                {
+                    Date = DateTime.Now,
+                    Status = "In Transit",
+                    Address = "Test Address"
+                };
+                context.Orders.Add(order);
+                context.SaveChanges();
+
+                // Finally create packages with valid foreign keys
+                var testPackages = new List<Package>
+                {
+                    new Package { Barcode = "PKG001", Weight = 2.5, OrderId = order.Id, CartId = cart.Id },
+                    new Package { Barcode = "PKG002", Weight = 1.2, OrderId = order.Id, CartId = cart.Id },
+                    new Package { Barcode = "PKG003", Weight = 3.8, OrderId = order.Id, CartId = cart.Id },
+                    new Package { Barcode = "PKG004", Weight = 0.9, OrderId = order.Id, CartId = cart.Id },
+                    new Package { Barcode = "PKG005", Weight = 4.2, OrderId = order.Id, CartId = cart.Id }
+                };
+
+                context.Packages.AddRange(testPackages);
+                context.SaveChanges();
+            }
         }
     }
 }
--- BezorgApplicatie/Views/PakkettenInscannen.xaml.cs
+++ BezorgApplicatie/Views/PakkettenInscannen.xaml.cs
@@ -1,45 +1,148 @@
 using SQLite;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using ZXing;
 using ZXing.Net.Maui;
 using ZXing.Net.Maui.Controls;
+using BezorgApplicatie.Data;
+using BezorgApplicatie.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BezorgApplicatie.Views;
-public partial class PakkettenInscannen : ContentPage
+
+public class ScannedPackageViewModel
 {
-    private ObservableCollection<string> scannedBarcodes;
+    public Package Package { get; set; }
+    public string BarcodeDisplay => Package?.Barcode ?? "N/A";
+    public string WeightDisplay => Package?.Weight.ToString("F2") + " kg" ?? "N/A";
+    public int Id => Package?.Id ?? 0;
+}
 
-    public PakkettenInscannen()
+public partial class PakkettenInscannen : ContentPage, INotifyPropertyChanged
     {
-        InitializeComponent();
-        scannedBarcodes = new ObservableCollection<string>();
-        BindingContext = this;
+        private ObservableCollection<ScannedPackageViewModel> scannedBarcodes;
+        private string selectedBarcode;
+        private DataContext _dataContext;
+        private int _scannedCount = 0;
+        private int _maxPackagesPerZone = 5;
+        private List<string> _zones = new() { "D", "C", "B", "A" };
+        private int _currentZoneIndex = 0;
+        private string _currentZone = "D";
+        private string _counterDisplay = "0/5";
 
-        barcodeReader.Options = new BarcodeReaderOptions
-        {
-            Formats = BarcodeFormats.All,
-            AutoRotate = true,
-            TryHarder = true
-        };
-
-        barcodeReader.BarcodesDetected += OnBarcodesDetected;
-    }
-
-    public ObservableCollection<string> ScannedBarcodes => scannedBarcodes;
-
-    public void OnBarcodesDetected(object sender, BarcodeDetectionEventArgs e)
-    {
-        MainThread.BeginInvokeOnMainThread(() =>
-        {
-            foreach (var barcode in e.Results)
-            {
-                if (!scannedBarcodes.Contains(barcode.Value))
+        public string CurrentZone 
+        { 
+            get => _currentZone; 
+            set 
+            { 
+                if (_currentZone != value)
                 {
-                    scannedBarcodes.Add(barcode.Value);
+                    _currentZone = value;
+                    OnPropertyChanged();
                 }
             }
-        });
+        }
+
+        public string CounterDisplay 
+        { 
+            get => _counterDisplay; 
+            set 
+            { 
+                if (_counterDisplay != value)
+                {
+                    _counterDisplay = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public PakkettenInscannen()
+        {
+            InitializeComponent();
+            scannedBarcodes = new ObservableCollection<ScannedPackageViewModel>();
+            BindingContext = this;
+
+            barcodeReader.Options = new BarcodeReaderOptions
+            {
+                Formats = BarcodeFormats.All,
+                AutoRotate = true,
+                TryHarder = true
+            };
+
+            barcodeReader.BarcodesDetected += OnBarcodesDetected;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            try
+            {
+                _dataContext = ServiceHelper.GetService<DataContext>();
+                if (_dataContext == null)
+                {
+                    await DisplayAlert("Error", "Database context niet geïnitialiseerd", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", $"Failed to initialize database: {ex.Message}", "OK");
+            }
+        }
+
+        public ObservableCollection<ScannedPackageViewModel> ScannedBarcodes => scannedBarcodes;
+
+    public void OnBarcodesDetected(object sender, BarcodeDetectionEventArgs e)
+        {
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                if (_dataContext == null)
+                {
+                    await DisplayAlert("Error", "Database context is nog niet geïnitialiseerd", "OK");
+                    return;
+                }
+
+                foreach (var barcode in e.Results)
+                {
+                    if (!scannedBarcodes.Any(p => p.BarcodeDisplay == barcode.Value))
+                    {
+                        try
+                        {
+                            var package = await _dataContext.Packages
+                                .AsNoTracking()
+                                .FirstOrDefaultAsync(p => p.Barcode == barcode.Value);
+
+                            if (package != null)
+                            {
+                                scannedBarcodes.Add(new ScannedPackageViewModel { Package = package });
+                                UpdateCounter();
+                            }
+                            else
+                            {
+                                await DisplayAlert("Info", $"Package met barcode {barcode.Value} niet gevonden in database", "OK");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            await DisplayAlert("Error", $"Database error: {ex.Message}\n\nStackTrace: {ex.InnerException?.Message}", "OK");
+                        }
+                    }
+                }
+            });
+        }
+
+    private void UpdateCounter()
+    {
+        _scannedCount = scannedBarcodes.Count;
+        CounterDisplay = $"{_scannedCount}/{_maxPackagesPerZone}";
     }
 
     public static class Constants
@@ -58,18 +161,57 @@ public partial class PakkettenInscannen : ContentPage
             Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
     }
 
-    public void ProbleemMelden(object sender, EventArgs e)
+    public async void ProbleemMelden(object sender, EventArgs e)
     {
-        return;
+        if (sender is Button button && button.CommandParameter is ScannedPackageViewModel packagevm)
+        {
+            var barcode = packagevm.BarcodeDisplay;
+            await Shell.Current.GoToAsync($"probleem-melden?barcode={barcode}");
+        }
     }
 
-    public void VolgendeZone(object sender, EventArgs e)
+    public async void VolgendeZone(object sender, EventArgs e)
     {
-        return;
+        if (_scannedCount >= _maxPackagesPerZone)
+        {
+            if (_currentZoneIndex < _zones.Count - 1)
+            {
+                _currentZoneIndex++;
+                CurrentZone = _zones[_currentZoneIndex];
+
+                scannedBarcodes.Clear();
+                _scannedCount = 0;
+                UpdateCounter();
+
+                await DisplayAlert("Zone", $"Volgende zone: {CurrentZone}", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Info", "Alle zones zijn afgerond!", "OK");
+            }
+        }
+        else
+        {
+            await DisplayAlert("Info", $"Scan eerst alle pakketten. ({_scannedCount}/{_maxPackagesPerZone})", "OK");
+        }
     }
 
-    public void VorigeZone(object sender, EventArgs e)
+    public async void VorigeZone(object sender, EventArgs e)
     {
-        return;
+        if (_currentZoneIndex > 0)
+        {
+            _currentZoneIndex--;
+            CurrentZone = _zones[_currentZoneIndex];
+
+            scannedBarcodes.Clear();
+            _scannedCount = 0;
+            UpdateCounter();
+
+            await DisplayAlert("Zone", $"Vorige zone: {CurrentZone}", "OK");
+        }
+        else
+        {
+            await DisplayAlert("Info", "Dit is de eerste zone", "OK");
+        }
     }
 }
